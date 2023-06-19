027de886b4543f7863f1b8f05efc6f98ff4865ec
thomasmichaelwallace <thomasmichaelwallace@gmail.com>
2023-06-19 22:42:14 +0100

    save and recover payloads

---
+++ lib/common/payload.ts
@@ -0,0 +1,64 @@
+const uint8ToBase64 = (arr: Uint8Array): string => btoa(
+  Array(arr.length)
+    .fill('')
+    .map((_, i) => String.fromCharCode(arr[i]))
+    .join(''),
+);
+const base64ToUint8 = (str: string): Uint8Array => Uint8Array
+  .from(atob(str), (c) => c.charCodeAt(0));
+
+async function compressArrayBuffer(input: ArrayBuffer) {
+  const cs = new CompressionStream('deflate');
+  const writer = cs.writable.getWriter();
+  writer.write(input);
+  writer.close();
+  const output = [];
+  const reader = cs.readable.getReader();
+  let totalSize = 0;
+  while (true) {
+    const { value, done } = await reader.read();
+    if (done) { break; }
+    output.push(value);
+    totalSize += value.byteLength;
+  }
+  const concatenated = new Uint8Array(totalSize);
+  let offset = 0;
+  // eslint-disable-next-line no-restricted-syntax
+  for (const array of output) {
+    concatenated.set(array, offset);
+    offset += array.byteLength;
+  }
+  return concatenated;
+}
+
+async function decompressArrayBuffer(input: ArrayBuffer) {
+  const cs = new DecompressionStream('deflate');
+  const writer = cs.writable.getWriter();
+  writer.write(input);
+  writer.close();
+  const output = [];
+  const reader = cs.readable.getReader();
+  let totalSize = 0;
+  while (true) {
+    const { value, done } = await reader.read();
+    if (done) { break; }
+    output.push(value);
+    totalSize += value.byteLength;
+  }
+  const concatenated = new Uint8Array(totalSize);
+  let offset = 0;
+  // eslint-disable-next-line no-restricted-syntax
+  for (const array of output) {
+    concatenated.set(array, offset);
+    offset += array.byteLength;
+  }
+  return concatenated;
+}
+
+export async function getSaveCode(memory: Uint8Array): Promise<string> {
+  return uint8ToBase64(await compressArrayBuffer(memory));
+}
+
+export async function fromSaveCode(saveCode: string): Promise<Uint8Array> {
+  return decompressArrayBuffer(base64ToUint8(saveCode));
+}
--- viewers/local.ts
+++ viewers/local.ts
@@ -1,19 +1,11 @@
+/* eslint-env browser */
+
 /* eslint-disable no-console */
 import Doom, { KeyCodes, KeyEvent } from '../lib/common/doom';
+import { fromSaveCode, getSaveCode } from '../lib/common/payload';
 import toDoomKey from '../lib/common/toDoomKey';
 // @ts-expect-error doomWasm is a string
 import doomWasmName from '../tmp/doom.wasm';
-
-let DUMP: Uint8Array;
-
-const uint8ToBase64 = (arr: Uint8Array): string => btoa(
-  Array(arr.length)
-    .fill('')
-    .map((_, i) => String.fromCharCode(arr[i]))
-    .join(''),
-);
-const base64ToUint8 = (str: string): Uint8Array => Uint8Array
-  .from(atob(str), (c) => c.charCodeAt(0));
 
 async function main() {
   const canvas = document.getElementById('doom-frame') as HTMLCanvasElement;
@@ -33,26 +25,31 @@ async function main() {
   };
 
   const dumpGameButton = document.getElementById('doom-dump') as HTMLAnchorElement;
-  dumpGameButton.onclick = () => {
-    DUMP = new Uint8Array(doom.memory.buffer, 0, doom.memory.buffer.byteLength).slice();
-    console.log('dumped', DUMP.byteLength);
-    console.log('saveKey', uint8ToBase64(DUMP));
+  dumpGameButton.onclick = async () => {
+    const dump = new Uint8Array(doom.memory.buffer, 0, doom.memory.buffer.byteLength);
+    const code = await getSaveCode(dump);
+    console.log('-- saveKey > --');
+    console.log(code);
+    console.log('-- < saveKey --');
   };
   const recoverGameButton = document.getElementById('doom-recover') as HTMLAnchorElement;
-  recoverGameButton.onclick = () => {
-    // @ts-expect-error base64Dump is a string
-    if (window.base64Dump !== undefined) {
-      // @ts-expect-error base64Dump is a string
-      DUMP = base64ToUint8(window.base64Dump as string);
-      console.log('recovered', DUMP.length);
-    }
-    if (DUMP === undefined) {
-      console.warn('No dump');
+  recoverGameButton.onclick = async () => {
+    const { saveKey } = window as unknown as { saveKey?: string };
+    if (saveKey === undefined) {
+      console.warn('window.saveKey must be set');
       return;
     }
-    const bytes = new Uint8Array(doom.memory.buffer, 0, DUMP.length);
-    bytes.set(DUMP);
-    console.log('recovered', DUMP.length, bytes);
+    const dump = await fromSaveCode(saveKey);
+    if (doom.memory.buffer.byteLength < dump.length) {
+      console.warn('doom.memory.buffer is too small');
+      const delta = (dump.length - doom.memory.buffer.byteLength) / 65536; // in 64k pages
+      console.log('growing doom.memory.buffer by', delta, 'pages');
+      doom.memory.grow(Math.ceil(delta));
+    }
+
+    const memory = new Uint8Array(doom.memory.buffer, 0, dump.length);
+    memory.set(dump);
+    console.log('recovered', dump.length);
   };
 
   doom.updateScreen = (img) => {
